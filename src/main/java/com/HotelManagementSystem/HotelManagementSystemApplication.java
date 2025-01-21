6a127fa76422c801a8b3c1ce1fb538c41cdd1340
Ganesh9554 <ganeshroyal67@gmail.com>
2025-01-21 13:16:36 +0530

    latest code

--- src/main/java/com/HotelManagementSystem/HotelManagementSystemApplication.java
+++ src/main/java/com/HotelManagementSystem/HotelManagementSystemApplication.java
@@ -2,11 +2,7 @@ package com.HotelManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-@ComponentScan({"com.HotelManagementSystem.Controller","com.HotelManagementSystem.Repository"})
-@EnableJpaRepositories("com.HotelManagementSystem.Repository")
-@SpringBootApplication(scanBasePackages = {"Controller","Service","Model", "Repository"})
+@SpringBootApplication
 public class HotelManagementSystemApplication {
 
 	public static void main(String[] args) {
