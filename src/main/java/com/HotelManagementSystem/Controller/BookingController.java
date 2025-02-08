baeb6b55994614997c0e9bbe376e74420fbc4168
Ganesh9554 <ganeshroyal67@gmail.com>
2025-02-08 16:10:27 +0530

    review comments fixed

--- src/main/java/com/HotelManagementSystem/Controller/BookingController.java
+++ src/main/java/com/HotelManagementSystem/Controller/BookingController.java
@@ -39,7 +39,7 @@ public class BookingController {
 	@PostMapping("/savebooking")
 	public ResponseEntity<BookingResponse> bookHotel(@RequestBody Booking booking) 
 			throws RoomNotFoundException,  CustomerNotFoundException{
-		log.debug("bookHotel :: Booking: {}", booking);
+		log.debug("Begin: bookHotel :: Booking: {}", booking);
 		BookingResponse br=null;
 		String cname=booking.getCustomerName();
 		Optional<Customer> customer=customerRepository.findByCustomerName(cname);
